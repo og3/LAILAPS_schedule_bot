ae8ba3b1efb0802d11f68b0df8d0671fd16b231f
noriyuki togashi <>
2019-10-16 15:39:04 +0900

    バグ修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -12,7 +12,7 @@ class LinebotController < ApplicationController
     end
   
     def answer_for_word_of_now
-        lesson_of_now = Lesson.find_by(day_of_the_week: Time.current.wday, start_on: [-Float::INFINITY..Time.current.strftime('%H%M').to_i]
+        lesson_of_now = Lesson.find_by(day_of_the_week: Time.current.wday, start_on: [-Float::INFINITY..Time.current.strftime('%H%M').to_i])
         lesson_of_next = Lesson.find_by(day_of_the_week: Time.current.wday, number_of_lessons: @lesson_of_now.number_of_lessons + 1)
         @message = "現在のレッスンは、#{lesson_of_now.name}です。\nトレーナー：#{lesson_of_now.trainer}\n時間：#{lesson_of_now.start_on}〜#{lesson_of_now.end_on}まで\n\n次回のレッスンは#{lesson_of_next.name}です\nトレーナー：#{lesson_of_next.trainer}\n時間：#{lesson_of_next.start_on}〜#{lesson_of_next.end_on}まで"
     end
