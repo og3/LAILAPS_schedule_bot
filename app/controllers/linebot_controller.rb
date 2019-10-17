ef468b52b8426c2e0ad51871c5c91098dc1a0054
noriyuki togashi <>
2019-10-17 13:58:10 +0900

    修正

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -14,7 +14,7 @@ class LinebotController < ApplicationController
     def answer_for_word_of_now
         lesson_of_now = Lesson.find_by(day_of_the_week: Time.current.wday, start_on: [-Float::INFINITY..Time.current.strftime('%H%M').to_i])
         lesson_of_next = Lesson.find_by(day_of_the_week: Time.current.wday, number_of_lessons: lesson_of_now.number_of_lessons + 1)
-        @message = "現在のレッスンは、#{lesson_of_now.name}です。\nトレーナー：#{lesson_of_now.trainer}\n時間：#{lesson_of_now.start_on.strftime('%H：%M')}〜#{lesson_of_now.end_on.strftime('%H：%M')}まで\n\n次回のレッスンは、#{lesson_of_next.name}です\nトレーナー：#{lesson_of_next.trainer}\n時間：#{lesson_of_next.start_on.strftime('%H：%M')}〜#{lesson_of_next.end_on.strftime('%H：%M')}まで"
+        @message = "現在のレッスンは、#{lesson_of_now.name}です。\nトレーナー：#{lesson_of_now.trainer}\n時間：#{lesson_of_now.start_on.strftime('%H：%M')}〜#{lesson_of_now.end_on.strftime('%H：%M')}\n\n次回のレッスンは、#{lesson_of_next.name}です\nトレーナー：#{lesson_of_next.trainer}\n時間：#{lesson_of_next.start_on.strftime('%H：%M')}〜#{lesson_of_next.end_on.strftime('%H：%M')}"
         @message
     end
 
